e075b81e57d801d5aa3c0ab004cc17508107a619
zbnfy <352632597@qq.com>
2017-09-30 15:41:12 +0800

    修改后

--- src/main/java/com/njq/controller/SpringController.java
+++ src/main/java/com/njq/controller/SpringController.java
@@ -11,9 +11,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * Created by NIJIAQI on 2017/9/30.
  */
 @Controller
-@RequestMapping(value="/hello", method = GET)
+@RequestMapping("/hello")
 public class SpringController {
-    @RequestMapping(value="/hello", method = GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
